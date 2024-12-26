00d410f9d4d881766e5a15c292a2f482188de711
natthanaphop-isa <58567478+natthanaphop-isa@users.noreply.github.com>
2024-12-26 22:49:33 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -220,7 +220,7 @@ def main():
                     }
                 
                 if results:
-                    st.subheader(f"{ticker} Analysis between {start_date.strftime(f"%b")}, {start_date.year} to {end_date.strftime(f"%b")}, {end_date.year}")
+                    st.subheader(f"{ticker} Analysis between {start_date.strftime('%b')}, {start_date.year} to {end_date.strftime('%b')}, {end_date.year}")
                     for ticker, metrics in results.items():
                         st.write(f"### {ticker}")
                         for metric, value in metrics.items():
