4cbb105d544f222d4d6fec5113d42c285468aef5
natthanaphop-isa <58567478+natthanaphop-isa@users.noreply.github.com>
2024-12-26 22:58:59 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -228,7 +228,7 @@ def main():
                             if isinstance(value, float):
                                 if 'CAGR' or 'Risk' or "Annual" or 'Drawdown' in metric:
                                     st.write(f"{metric}: {value * 100:.2f}%")
-                                elif 'Recovery' in metric:
+                                elif 'Months' in metric:
                                     st.write(f"{metric}: {value:.2f} Months")
                                 else:
                                     st.write(f"{metric}: {value:.2f}")
